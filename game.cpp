ce8fadde3c9238404f2fe4ca363ac0bd3c2160e0
Sarkis Karapetyan <32280446+Sargis24@users.noreply.github.com>
2019-09-20 00:28:24 -0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,15 +1,10 @@
-//Sargis Karapetyan
-//CSIS 137
-//HW1
-
 #include "TicTacToe.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main()
-{
+int main(){
 	int row, col, player = 0;
 	int temp = 0;
 	int winner = 0;
@@ -18,16 +13,14 @@ int main()
 	string player1, player2;
 	TicTacToe game;
 
-	do
-	{
+	do{
 		cout << "Welcome to the Tic Tac Toe game." << endl
 			<< "Press 1, to play." << endl
 			<< "Press 'Q' to quit." << endl;
 		cin >> selection;
 		cin.ignore();
 
-		if (selection == '1')
-		{
+		if (selection == '1'){
 			TicTacToe();
 
 			cout << "\nPlease enter 1st player name" << endl;
@@ -37,13 +30,11 @@ int main()
 			cout << endl;
 
 
-			do
-			{
+			do{
 				game.displayTable();
 				cout << endl << player1 << ", please enter the number of ROW (1 to 3)" << endl;
 				cin >> temp;
-				while (temp < 1 || temp>3)
-				{
+				while (temp < 1 || temp>3){
 					cout << "The number of rows cannot be less than 1 and greater than 3." << endl;
 					cout << "Please reselct the row." << endl;
 					cin >> temp;
@@ -52,16 +43,14 @@ int main()
 				cout << player1 << ", please enter the COLUMN (1 to 3)" << endl;
 				cin >> temp;
 				cout << endl;
-				while (temp < 1 || temp>3)
-				{
+				while (temp < 1 || temp>3){
 					cout << "The number of rows cannot be less than 1 and greater than 3." << endl;
 					cout << "Please reselct the row." << endl;
 					cin >> temp;
 				}
 				col = temp - 1;
 				cout << endl;
-				while (game.setTable(1, row, col) == false)
-				{
+				while (game.setTable(1, row, col) == false){
 					cout << "That box is taken. Please reselect the box." << endl;
 					cout << player1 << ", please enter the number of ROW (1 to 3)" << endl;
 					cin >> temp;
@@ -70,21 +59,17 @@ int main()
 					cin >> temp;
 					col = temp - 1;
 				}
-				if (game.isWin(1) == 1)
-				{
+				if (game.isWin(1) == 1)	{
 					winner = 1;
 					cout << endl << player1 << ", WINS!" << endl;
 				}
-				else
-				{
+				else{
 					draw--;
-					if (draw == 0)
-					{
+					if (draw == 0){
 						winner = 1;
 						cout << "It's a TIE.\n" << endl;
 					}
-					else
-					{
+					else{
 						game.displayTable();
 						cout << endl<<player2 << ", please enter the number of ROW (1 to 3)" << endl;
 						cin >> temp;
@@ -93,8 +78,7 @@ int main()
 						cin >> temp;
 						cout << endl;
 						col = temp - 1;
-						while (game.setTable(2, row, col) == false)
-						{
+						while (game.setTable(2, row, col) == false){
 							cout << "That box is taken. Please reselect the box." << endl;
 							cout << player2 << ", please enter the number of ROW (1 to 3)" << endl;
 							cin >> temp;
@@ -103,14 +87,12 @@ int main()
 							cin >> temp;
 							col = temp - 1;
 						}
-						if (game.isWin(2) == 1)
-						{
+						if (game.isWin(2) == 1)	{
 							winner = 2;
 							cout << endl<<player2 << ", WINS!" << endl;
 							game.clearTable();
 						}
-						else
-						{
+						else{
 							draw--;
 							if (draw == 0)
 							{
@@ -124,8 +106,7 @@ int main()
 				}
 			} while (winner == 0);
 		}
-		else if (selection != '1' && selection != 'q' && selection != 'Q')
-			{
+		else if (selection != '1' && selection != 'q' && selection != 'Q'){
 				cout << "Invalid selection. Try again." << endl;
 			}
 		game.clearTable();
